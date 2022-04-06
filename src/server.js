454469db190167755103a5754e32d1d673f41a57
mulori <murilogarcia.hr@gmail.com>
2022-04-05 21:36:12 -0300

    new route reward

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const routeActivity = require('./routes/routeActivity');
 const routeUser = require('./routes/routeUser');
 const routeConfig = require('./routes/routeConfig');
 const routeWelcome = require('./routes/routeWelcome');
+const routeReward = require('./routes/routeReward');
 const app = express();
 
 app.use(express.json());
@@ -14,5 +15,6 @@ app.use(routeActivity);
 app.use(routeUser);
 app.use(routeConfig);
 app.use(routeWelcome);
+app.use(routeReward);
 
 app.listen(process.env.PORT || 8182);
