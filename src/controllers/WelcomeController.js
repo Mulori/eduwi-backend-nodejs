2e95b9a7754ed22fcaa4ce1bd68ed5bc93437593
mulori <murilogarcia.hr@gmail.com>
2022-06-02 22:44:04 -0300

    alterado route welcome

--- src/controllers/WelcomeController.js
+++ src/controllers/WelcomeController.js
@@ -2,7 +2,7 @@
 module.exports = {
     async Welcome(req, res) {        
         return res.json({ 
-            eduwi: 'Welcome to backend service eduwi',
+            eduwi: 'Welcome to backend service eduwi - Bem vindo ao serviço de backend eduwi',
             created: 'Service created in 2022-01-29',
             developed_by: 'Murilo Henrique Garcia Rodrigues',
             developer_nationality: 'Brazil',            
